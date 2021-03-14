a96aef3d7e4e77048f3a26f8ad670e1ba97abb4c
MaksimWeb <sayamonkoh@gmail.com>
2021-03-14 16:34:34 +1000

    changes applications

--- computers/parsing.py
+++ computers/parsing.py
@@ -1,6 +1,12 @@
 from .models import Application, Computer
 
 
+class App:
+    def __init__(self, application_name, application_version):
+        self.application_name = application_name
+        self.application_version = application_version
+
+
 def parsing():
     file = open('C:/Users/Максим/Desktop/1.txt')
     line = file.readline()
@@ -35,3 +41,33 @@ def parsing():
 
         comp.applications.set(apps)
         comp.save()
+    else:
+        newApps = []
+        oldApp = Application.objects.filter(computer_id=comp.id)
+
+        while line := file.readline():
+            if '-' not in line:
+                continue
+            splitted_line = line.split('-')
+            updatedApp = App(application_name=splitted_line[0], application_version=splitted_line[1] or None)
+            newApps.append(updatedApp)
+
+        newAppsCopy = newApps.copy()
+
+        for newItem in newApps:
+            for oldItem in oldApp:
+                if newItem.application_name != oldItem.application_name:
+                    continue
+                elif newItem.application_version != oldItem.application_version:
+                    oldItem.application_version = newItem.application_version
+                    oldItem.save()
+
+        for oldItem in oldApp:
+            for newItem in newApps:
+                if oldItem.application_name == newItem.application_name:
+                    newAppsCopy.remove(newItem)
+
+        for refresh in newAppsCopy:
+            if len(newAppsCopy) > 0:
+                Application.objects.create(application_version=refresh.application_version, computer_id=comp.id,
+                                           application_name=refresh.application_name)
