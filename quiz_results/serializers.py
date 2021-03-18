20e4f9d432f56d8d95287f22f4a3d0e6345e09bc
MaksimWeb <sayamonkoh@gmail.com>
2021-03-19 00:16:15 +1000

    result model connected with others

--- quiz_results/serializers.py
+++ quiz_results/serializers.py
@@ -1,9 +1,25 @@
-
 from .models import Result
+from quizes.models import Quiz
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
+class QuizS(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ('id', 'name', 'topic',)
+
+
+class UserS(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
+
+
 class ResultSerializer(serializers.ModelSerializer):
+    quiz = QuizS(read_only=True)
+    user = UserS(read_only=True)
+
     class Meta:
         fields = ('quiz', 'user', 'score', 'created')
         model = Result
